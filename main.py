94ceb8d053e107a657cf29684dc8c4fdb3ab92eb
Jorge Alvarenga <jorgeluizalvarenga@gmail.com>
2024-03-15 15:45:36 -0300

    Mais print

--- main.py
+++ main.py
@@ -11,3 +11,6 @@ repo = database_count.ConfigCircuitoRepository()
 repo.insert(circuito="P1-MQA3-CH0",dt_insert='19/04/1988',camera_id='1',bound_box="[198,166,122]",count=1,upload=False)
 
 print('teste')
+print('teste')
+print('teste')
+print('teste')
